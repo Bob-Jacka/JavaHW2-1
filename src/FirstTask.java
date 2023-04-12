c23933b02479d7d5528e0fcf301ce1df9e4dbdec
Kirill <evilprog@yandex.ru>
2023-04-12 19:41:37 +0400

    resolved tasks

---
+++ src/FirstTask.java
@@ -0,0 +1,9 @@
+public class FirstTask {
+    public static void main(String[] args) {
+
+        int inputPriceTicket = 13_676;
+        int outBonusVal = inputPriceTicket / 20;
+
+        System.out.println(outBonusVal);
+    }
+}
